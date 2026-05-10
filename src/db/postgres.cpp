a9dc35a277f60278ff066441d005458bcb6bd476
Joshua Liu <liu.joshua555@gmail.com>
2026-05-10 11:27:02 -0500

    fixed exec warnings; added stdexcept

--- src/db/postgres.cpp
+++ src/db/postgres.cpp
@@ -2,6 +2,8 @@
 
 #include "postgres.hpp"
 
+#include <pqxx/pqxx>
+
 #include "../util/convert.hpp" 
 
 Postgres::Postgres(const std::string& conn_str) : conn(conn_str) {}
@@ -21,36 +23,60 @@ pqxx::result Postgres::query(const std::string& sql) {
 
 void Postgres::insert_hospital(const Hospital &h) {
   pqxx::work txn(conn);
-  txn.exec_params(
+  txn.exec(
     "INSERT INTO hospitals (id, num_patients, capacity, lat, lon) VALUES ($1, $2, $3, $4, $5)",
-    h.id, h.num_patients, h.capacity, h.location.lat, h.location.lon
+    pqxx::params{
+      h.id,
+      h.num_patients,
+      h.capacity,
+      h.location.lat,
+      h.location.lon
+    }
   );
   txn.commit();
 }
 
 void Postgres::insert_ambulance(const Ambulance &a) {
   pqxx::work txn(conn);
-  txn.exec_params(
+  txn.exec(
     "INSERT INTO ambulances (id, status, type, lat, lon) VALUES ($1, $2, $3, $4, $5)",
-    a.id, to_string(a.ambulance_status), to_string(a.ambulance_type), a.location.lat, a.location.lon
+    pqxx::params{
+      a.id,
+      to_string(a.ambulance_status),
+      to_string(a.ambulance_type),
+      a.location.lat,
+      a.location.lon
+    }
   );
   txn.commit();
 }
 
 void Postgres::insert_call(const Call &c) {
   pqxx::work txn(conn);
-  txn.exec_params(
+  txn.exec(
     "INSERT INTO calls (id, call_hour, call_minute, priority, description, lat, lon) VALUES ($1, $2, $3, $4, $5, $6, $7)",
-    c.id, c.time.hour, c.time.minute, to_string(c.priority), c.description, c.location.lat, c.location.lon
+    pqxx::params{
+      c.id,
+      c.time.hour,
+      c.time.minute,
+      to_string(c.priority),
+      c.description,
+      c.location.lat,
+      c.location.lon
+    }
   );
   txn.commit();
 }
 
 void Postgres::insert_dispatch(const Dispatch &d) {
   pqxx::work txn(conn);
-  txn.exec_params(
+  txn.exec(
     "INSERT INTO dispatches (call_id, ambulance_id, hospital_id) VALUES ($1, $2, $3)",
-    d.call_id, d.ambulance_id, d.hospital_id
+    pqxx::params{
+      d.call_id,
+      d.ambulance_id,
+      d.hospital_id
+    }
   );
   txn.commit();
 }
@@ -62,9 +88,17 @@ void Postgres::insert_event(const Event &e) {
   std::optional<int> a_id = e.ambulance_id == -1 ? std::nullopt : std::optional<int>(e.ambulance_id);
   std::optional<int> h_id = e.hospital_id == -1 ? std::nullopt : std::optional<int>(e.hospital_id);
 
-  txn.exec_params(
+  txn.exec(
     "INSERT INTO events (event_hour, event_minute, event_type, call_id, ambulance_id, hospital_id) VALUES ($1, $2, $3, $4, $5, $6)",
-    e.time.hour, e.time.minute, to_string(e.event_type), c_id, a_id, h_id
+    pqxx::params{
+      e.time.hour,
+      e.time.minute,
+      to_string(e.event_type),
+      c_id,
+      a_id,
+      h_id
+    }
   );
+
   txn.commit();
 }
